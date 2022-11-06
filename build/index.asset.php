049d63c82365428aaa0efac7f14d62ade0402fce
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-11-06 05:32:48 +0500

    Using InnerBlocks to Create a Nested Block

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'a8537c364589da1097ad8e126dc35d47');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '937b86ab2904b7d9d406590f168ca453');
