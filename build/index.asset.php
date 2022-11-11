2759573f6cc482be8442ea6d3f2b902612db5cc1
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-11-12 02:48:19 +0500

    Blob URL edge case & memory optimization

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '915eb27466b1f1b28d97f90b96726c1b');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a828b98f16747051b9de7e1305b94d7d');
