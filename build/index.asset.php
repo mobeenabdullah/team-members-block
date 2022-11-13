ec690caefa751762a060a63d13e2492a19ca3439
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-11-13 22:51:03 +0500

    The Save function Markup for the Social Links

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '869821d2c84e3daa6bed176830b170ee');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7b705a2c3eba939e4a53b9024b4711fd');
